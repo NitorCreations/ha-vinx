7d5fb867fd9f2021ff4ebf9e5aee082c45832579
Sam Stenvall <sam.stenvall@nitor.com>
2024-09-09 13:05:55 +0300

    Query and store device information as runtime data

--- custom_components/vinx/__init__.py
+++ custom_components/vinx/__init__.py
@@ -1,26 +1,64 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.device_registry import DeviceInfo, format_mac
 
+from custom_components.vinx.const import DOMAIN
 from custom_components.vinx.lw3 import LW3
 
 PLATFORMS: list[Platform] = [Platform.MEDIA_PLAYER]
 
 
+@dataclass
+class DeviceInformation:
+    mac_address: str
+    product_name: str
+    device_info: DeviceInfo
+
+
+@dataclass
+class VinxRuntimeData:
+    lw3: LW3
+    device_information: DeviceInformation
+
+
+async def get_device_information(lw3: LW3) -> DeviceInformation:
+    async with lw3.connection():
+        mac_address = str(await lw3.get_property("/.MacAddress"))
+        product_name = str(await lw3.get_property("/.ProductName"))
+        firmware_version = str(await lw3.get_property("/.FirmwareVersion"))
+        serial_number = str(await lw3.get_property("/.SerialNumber"))
+
+        device_info = DeviceInfo(
+            identifiers={(DOMAIN, format_mac(mac_address))},
+            name=product_name,
+            manufacturer="Lightware",
+            model=product_name,
+            sw_version=firmware_version,
+            serial_number=serial_number,
+        )
+
+        return DeviceInformation(mac_address, product_name, device_info)
+
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up from a config entry."""
-    # Verify we can connect
+    lw3 = LW3(entry.data["host"], entry.data["port"])
+
     try:
-        device = LW3(entry.data["host"], entry.data["port"])
-        await device.connect()
+        # Store runtime information
+        async with lw3.connection():
+            device_information = await get_device_information(lw3)
+
+        # Store the lw3 as runtime data in the entry
+        entry.runtime_data = VinxRuntimeData(lw3, device_information)
     except ConnectionError as e:
         raise ConfigEntryNotReady("Unable to connect") from e
-
-    # Store the device as runtime data in teh entry
-    entry.runtime_data = device
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
--- custom_components/vinx/media_player.py
+++ custom_components/vinx/media_player.py
@@ -2,15 +2,15 @@ import logging
 
 from homeassistant.components.media_player import MediaPlayerEntity
 
-from custom_components.vinx import LW3
+from custom_components.vinx import VinxRuntimeData
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
     # Extract stored runtime data
-    lw3_device: LW3 = entry.runtime_data
-    _LOGGER.info(lw3_device)
+    runtime_data: VinxRuntimeData = entry.runtime_data
+    _LOGGER.info(f"Runtime data: {runtime_data}")
 
 
 class VinxEncoder(MediaPlayerEntity):
